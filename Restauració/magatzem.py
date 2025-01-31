88bff3889c5485d2b71ffde820cb2f31ed391136
Judith <judithaviles2005@gmail.com>
2025-01-31 18:25:19 +0100

    Update magatzem.py

--- Restauració/magatzem.py
+++ Restauració/magatzem.py
@@ -13,38 +13,38 @@ class Magatzem:
   #def organitzar(self, Producte):
     # Codi
 
-  def add(self, Contenidor):
-    pos = self.organitzar(Contenidor.get_pr(Producte))
+  def add(self, Contenidor, pos): #added pos parameter
+    #pos = self.organitzar(Contenidor.get_pr(Producte)) #removed this line
     Contenidor.set(pos)
     for prestatge in self.espai:
       if prestatge.get_pos() == pos:
         prestatge.add(Contenidor)
 
   def delete(self, Contenidor):
-    pos = Contenidor.get_pos
+    pos = Contenidor.get_pos() #added ()
     i = 0
     end = False
     while i < len(self.espai) and not end:
-      if self.espai(i).get_pos() == pos:
+      if self.espai[i].get_pos() == pos: #changed self.espai(i) to self.espai[i]
         end = True
-    self.espai(i).get_cont().delete(0)
+    self.espai[i].get_cont().remove(Contenidor) #changed .delete(0) to .remove(Contenidor)
 
   def set_vendas(self, producte, num):
     end = False
     for prestatge in self.espai:
-      contenidor = prestatge.get_cont()[0]
-      if contenidor.get_pr().get_nom() == producte.get_nom():
-         correcte = prestatge.get_cont()[0].set_vendas(num)
-         if correcte and prestatge.get_cont()[0].get_q() == 0:
-          contenidor = prestatge.get_cont()
-          self.delete(contenidor)
-         return correcte
+      if prestatge.get_cont(): #added if condition to check if the list is empty
+          contenidor = prestatge.get_cont()[0]
+          if contenidor.get_pr().get_nom() == producte.get_nom():
+              correcte = prestatge.get_cont()[0].set_vendas(num)
+              if correcte and prestatge.get_cont()[0].get_q() == 0:
+                  contenidor = prestatge.get_cont()[0] #added [0] to access the first element
+                  self.delete(contenidor)
+              return correcte
     return False
 
 class Despensa(Magatzem):
   def __init__(self):
     super().__init__()
-    x = 0
     for x in range (0,4):
       for y in range (0,12):
         prestatge = Prestatge((x,y))
@@ -65,6 +65,7 @@ class Prestatge:
     self.continguts = []
 
   def getq(self):
+    from functools import reduce #added import
     return reduce(lambda acc, contingut: acc + contingut.get_q(), self.continguts, 0)
 
   def get_pos(self):
@@ -75,11 +76,7 @@ class Prestatge:
 
   def add(self, Contenidor):
     self.quantitat = self.quantitat + Contenidor.get_q()
-    i = len(self.continguts)
-    self.continguts.append(Contenidor)
-    while i > 0:
-      self.continguts[i] = self.continguts[i-1]
-    self.continguts[0] = Contenidor
+    self.continguts.append(Contenidor) #simplified add function
 
   def __str__(self):
     txt = ''
@@ -94,6 +91,7 @@ class Contenidor:
   def __init__(self, quantitat, producte):
     self.quantitat = quantitat
     self.producte = producte
+    self.pos = None #initialize pos
 
   def get_pr(self):
     return self.producte
@@ -104,13 +102,15 @@ class Contenidor:
   def set(self, pos):
     self.pos = pos
   
+  def get_pos(self): #added get_pos method
+      return self.pos
+
   def set_vendas(self, num):
     if self.quantitat - num >= 0:
       self.quantitat = self.quantitat - num
       return True
     else:
       return False
-
 
 class Producte:
   def __init__(self, id, nom, preu, categoria, fred):
@@ -133,6 +133,9 @@ class Producte:
   def get_cat(self):
     return self.categoria
   
+  def get_vendas(self): #added get_vendas
+      return self.vendas
+
   def set_vendas(self, num):
     self.vendas = self.vendas + num
 
@@ -143,7 +146,4 @@ class Producte:
       return f"\nID: {self.id} \nNom: {self.nom} \nPreu: {self.preu} \nCategoria: {self.categoria} \nFred: No"
 
   def fred(self):
-    if self.fred:
-      return True
-    else:
-      return False
+    return self.fred #simplified fred function
